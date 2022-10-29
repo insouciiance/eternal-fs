cded9cfbe15a3cae4e603290d5ead657fe2d4dbb
Nikita Kovalenko <nikkv.ottf@gmail.com>
2022-10-29 22:43:35 +0200

    * simplify fsinfo

--- EternalFS/EternalFS.Library/Commands/Filesystem/FsinfoCommand.cs
+++ EternalFS/EternalFS.Library/Commands/Filesystem/FsinfoCommand.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Text;
-using EternalFS.Library.Extensions;
-using EternalFS.Library.Filesystem;
-
-namespace EternalFS.Library.Commands.Filesystem;
+﻿namespace EternalFS.Library.Commands.Filesystem;
 
 [Command("fsinfo", true)]
 [CommandDoc("Outputs the information about the filesystem being used.")]
@@ -12,12 +6,9 @@ public partial class FsinfoCommand
 {
     public CommandExecutionResult Execute(ref CommandExecutionContext context)
     {
-        using Stream fsStream = context.FileSystem.GetStream();
-        EternalFileSystemHeader header = fsStream.MarshalReadStructure<EternalFileSystemHeader>();
-
-        context.Writer.WriteLine($"Size: {header.Size}B");
-        context.Writer.WriteLine($"Created at: {new DateTime(header.CreatedAt)}");
-        context.Writer.WriteLine($"Name: {Encoding.UTF8.GetString(header.Name).TrimEnd('\0')}");
+        context.Writer.WriteLine($"Size: {context.FileSystem.Size}B");
+        context.Writer.WriteLine($"Created at: {context.FileSystem.CreatedAt}");
+        context.Writer.WriteLine($"Name: {context.FileSystem.Name}");
 
         return new();
     }
